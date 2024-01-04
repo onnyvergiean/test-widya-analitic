a60f023bdedd3b89541ecd5fe0443d0dbb00f06e
Onny Vergiean Saputra <onny.s@students.amikom.ac.id>
2024-01-04 21:56:55 +0700

    Add Home Page List Products

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,5 +1,53 @@
+import { useEffect, useState, useMemo } from 'react';
+
+import ProductsList from '../components/ProductsList';
+import { useDispatch, useSelector } from 'react-redux';
+import { asyncGetAllProducts } from '../states/products/action';
+import NoProductsFound from './NoProductsFound';
+import debounce from 'lodash.debounce';
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  const dispatch = useDispatch();
+  const [searchTerm, setSearchTerm] = useState('');
+  const allProducts = useSelector((state) => state.products?.products);
+
+  const handleSearch = useMemo(
+    () =>
+      debounce((value) => {
+        dispatch(
+          asyncGetAllProducts({ searchTerm: value, page: 1, limit: 10 })
+        );
+      }, 500),
+    [dispatch]
+  );
+
+  const onSubmitSearch = (e) => {
+    e.preventDefault();
+    handleSearch.cancel();
+    dispatch(asyncGetAllProducts({ searchTerm, page: 1, limit: 10 }));
+  };
+
+  useEffect(() => {
+    handleSearch(searchTerm);
+    return () => {
+      handleSearch.cancel();
+    };
+  }, [dispatch, searchTerm, handleSearch]);
+
+  return (
+    <>
+      {allProducts && allProducts.length > 0 ? (
+        <ProductsList
+          products={allProducts}
+          onSubmitSearch={onSubmitSearch}
+          searchTerm={searchTerm}
+          onChangeSearchTerm={setSearchTerm}
+        />
+      ) : (
+        <NoProductsFound />
+      )}
+    </>
+  );
 };
 
 export default HomePage;
