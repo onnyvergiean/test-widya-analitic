66d9a9979a8292991e4afb4eb8a407826a341759
Onny Vergiean Saputra <onny.s@students.amikom.ac.id>
2024-01-04 21:57:13 +0700

    Add Not Found Page

---
+++ client/src/pages/NoProductsFound.jsx
@@ -0,0 +1,15 @@
+const NoProductsFound = () => {
+  return (
+    <section className="bg-white py-[200px]">
+      <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+        <div className="mx-auto max-w-screen-sm text-center">
+          <p className="mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl ">
+            Sorry no Products Found
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default NoProductsFound;
