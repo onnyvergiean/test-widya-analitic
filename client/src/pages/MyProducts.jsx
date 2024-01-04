f150c824ab87da57ece454005d0b4224258f5c15
Onny Vergiean Saputra <onny.s@students.amikom.ac.id>
2024-01-04 21:57:03 +0700

    Add MyProducts list

---
+++ client/src/pages/MyProducts.jsx
@@ -0,0 +1,51 @@
+import { useState, useEffect, useMemo } from 'react';
+import ProductsList from '../components/ProductsList';
+import { useDispatch, useSelector } from 'react-redux';
+import { asyncGetMyProducts } from '../states/products/action';
+import NoProductsFound from './NoProductsFound';
+import debounce from 'lodash.debounce';
+
+const MyProductsPage = () => {
+  const dispatch = useDispatch();
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const myProducts = useSelector((state) => state.products.myProducts);
+
+  const handleSearch = useMemo(
+    () =>
+      debounce((value) => {
+        dispatch(asyncGetMyProducts({ searchTerm: value, page: 1, limit: 10 }));
+      }, 500),
+    [dispatch]
+  );
+
+  const onSubmitSearch = (e) => {
+    e.preventDefault();
+    handleSearch.cancel();
+    dispatch(asyncGetMyProducts({ searchTerm, page: 1, limit: 10 }));
+  };
+
+  useEffect(() => {
+    handleSearch(searchTerm);
+    return () => {
+      handleSearch.cancel();
+    };
+  }, [dispatch, searchTerm, handleSearch]);
+
+  return (
+    <>
+      {myProducts && myProducts.length > 0 ? (
+        <ProductsList
+          products={myProducts}
+          onSubmitSearch={onSubmitSearch}
+          searchTerm={searchTerm}
+          onChangeSearchTerm={setSearchTerm}
+        />
+      ) : (
+        <NoProductsFound />
+      )}
+    </>
+  );
+};
+
+export default MyProductsPage;
