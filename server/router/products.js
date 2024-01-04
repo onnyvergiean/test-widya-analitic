a1cf52070610511b021038cee9242a5b84d1e6db
Onny Vergiean Saputra <onny.s@students.amikom.ac.id>
2024-01-04 10:01:44 +0700

    change route for myProducts

--- server/router/products.js
+++ server/router/products.js
@@ -4,7 +4,7 @@ const controller = require('../app/controllers/api/v1/products');
 const { authenticateToken } = require('../utils/jwt');
 
 router.get('/v1/products', controller.getAllProducts);
-router.get('/v1/my-products', authenticateToken, controller.getMyProducts);
+router.get('/v1/products/me', authenticateToken, controller.getMyProducts);
 router.get('/v1/products/:id', controller.getProductById);
 router.post('/v1/products', authenticateToken, controller.createProduct);
 router.put('/v1/products/:id', authenticateToken, controller.updateProduct);
